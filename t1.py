4492974cd64dbc4fade9f23ae42ff222e53d9338
dima <petroff.dmit@gmail.com>
2022-02-04 16:07:46 +0300

    Решена задача 1

---
+++ t1.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+import pygame
+import requests
+
+map_request = "http://static-maps.yandex.ru/1.x/"
+params = {'ll': '133.377150,-26.974007',
+          'spn': '25.0,25.0',
+          'l': 'map'}
+
+response = requests.get(map_request, params=params)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
